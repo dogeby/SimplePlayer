957b8f12cbd87e2a4e974245d67ec521531adef7
dogeby <dogebt@gmail.com>
2022-05-10 18:57:41 +0900

    splashscreen 버그 해결 권한 요청 코드 작성

--- app/src/main/java/com/yang/simpleplayer/activities/MainActivity.kt
+++ app/src/main/java/com/yang/simpleplayer/activities/MainActivity.kt
@@ -5,46 +5,72 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yang.simpleplayer.R
 import com.yang.simpleplayer.activities.list.ListActivity
+import com.yang.simpleplayer.databinding.ActivityMainBinding
 
+// TODO: permission 요청 거절 2번 초과시 intent 사용해서 사용자를 설정으로 안내하기
 class MainActivity : AppCompatActivity() {
+    private var _binding:ActivityMainBinding? = null
+    private val binding:ActivityMainBinding get() = requireNotNull(_binding)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        splashScreen.setKeepOnScreenCondition{true}
-        showVideoFileList()
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        showListActivity()
     }
 
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()
         ){ isGranted:Boolean ->
             if(isGranted) {
-                startVideoFileListActivity()
+                startListActivity()
+            } else {
+                finish()
             }
         }
 
-    private fun showVideoFileList() {
+    private fun showListActivity() {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-        == PackageManager.PERMISSION_GRANTED) {
-            startVideoFileListActivity()
-        } else {
-            requestReadExternalStoragePermission()
-        }
+                    == PackageManager.PERMISSION_GRANTED) {
+                        startListActivity()}
+        else requestReadExternalStoragePermission()
     }
 
     private fun requestReadExternalStoragePermission() {
-        requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
+            showPermissionAlertDialog()
+        } else {
+            showPermissionAlertDialog()
+        }
     }
 
-    private fun startVideoFileListActivity() {
+    private fun startListActivity() {
         val intent = Intent(this, ListActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun showPermissionAlertDialog() {
+        val alertDialog:AlertDialog? = this.let {
+            val builder = AlertDialog.Builder(it)
+            builder.apply {
+                setMessage(R.string.permission_message)
+                setPositiveButton(R.string.ok) { _, _ ->
+                    requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+                setNegativeButton(R.string.finish) { _, _ ->
+                    finish()
+                }
+            }
+            builder.create()
+        }
+        alertDialog?.show()
     }
 }
